cc828c6230fa80059b7496b0dd85c75656e521cc
JatinShivlani <Jatinshivlani272@gmail.com>
2025-04-24 16:49:19 +0530

    ride logic completed

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const cors = require("cors");
-const connectToDb= require('./db/db')
-const cookieParser = require("cookie-parser");
-const captainRoutes = require('./routes/captain.routes');
-app.use(cors());
-// connecting to db
-connectToDb();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
+connectToDb();
 
-// for parsing application/json(body-parser)
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// for parsing cookies
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
-// routes
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const app = require('./app')
-
+const http = require('http');
+const app = require('./app');
+const { initializeSocket } = require('./socket');
 const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
-    console.log(`server listning on port ${port}`)
-})
+const server = http.createServer(app);
+
+initializeSocket(server);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
